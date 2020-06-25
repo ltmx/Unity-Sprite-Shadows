ee8566b89a39c61cfdc4ce2b8240377c1d19b81c
LTMX <47640688+LTMX@users.noreply.github.com>
2020-06-25 10:20:56 +0200

    Initial Commit

---
+++ SpriteShadows.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace VFX.Sprite_Shadows
+{
+    [ExecuteAlways, RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteShadows : MonoBehaviour
+    {
+        public ShadowCastingMode shadowCastingMode = ShadowCastingMode.On;
+        public MotionVectorGenerationMode motionVectorGenerationMode = MotionVectorGenerationMode.Object;
+        public bool receiveShadows = true;
+        private SpriteRenderer sr;
+        //private Canvas
+        private void Start()
+        {
+            sr = GetComponent<SpriteRenderer>();
+            UpdateSpriteParameters();
+        }
+
+        [ContextMenu("Update Sprite Parameters")]
+        public void UpdateSpriteParameters()
+        {
+            if (sr == null) return;
+            sr.receiveShadows = receiveShadows;
+            sr.shadowCastingMode = shadowCastingMode;
+            sr.motionVectorGenerationMode = motionVectorGenerationMode;
+        }
+    }
+}
